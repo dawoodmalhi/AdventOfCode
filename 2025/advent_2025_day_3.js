7d01efae197c23c162e983875c82feff883fc041
dawoodmalhi <dawoodmalhi@outlook.com>
2025-12-04 12:39:41 +0500

    2025 Day 3 Puzzle

--- 2025/advent_2025_day_3.js
+++ 2025/advent_2025_day_3.js
@@ -14,6 +14,23 @@ function maxJoltageInBankP1(bank) {
   return maxBankjoltage;
 }
 
+// Monotonic Stack approach
+function maxJoltageInBankP2(bank) {
+  const toRemove = bank.length - 12; 
+  let remove = toRemove;
+  const stack = [];
+
+  for (let digit of bank) {
+      while (remove > 0 && stack.length > 0 && stack[stack.length - 1] < digit) {
+          stack.pop();
+          remove--;
+      }
+      stack.push(digit);
+  }
+
+  return parseInt(stack.slice(0, 12).join(''));
+}
+
 async function maxOutputJoltage(filePath) {
   let combinedJoltage = 0;
 
@@ -22,11 +39,13 @@ async function maxOutputJoltage(filePath) {
   const banks = data.split("\n");
 
   for (let i = 0; i < banks.length; i++)
-    combinedJoltage += maxJoltageInBankP1(banks[i]);
+    combinedJoltage += maxJoltageInBankP2(banks[i]);
 
   return combinedJoltage;
 }
 
-maxOutputJoltage("advent_2025_day_3_input.txt").then((sum) =>
-  console.log(sum)
+maxOutputJoltage("advent_2025_day_3_input.txt").then((joltage) =>
+  console.log(joltage)
 );
+
+// maxJoltageInBankP2('234234234234278')
